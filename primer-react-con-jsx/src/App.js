919a085baea6774980a81720c040011b298825e6
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-10-03 23:50:16 -0300

    Corrección
    Se corrige indentación del código.

--- primer-react-con-jsx/src/App.js
+++ primer-react-con-jsx/src/App.js
@@ -1,9 +1,9 @@
 import { Component } from "react";
 import ListaTareas from "./Componentes/listaTareas.js"
 
-class App extends Component{
-  render (){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div>
         <h1>Hello Dojo!</h1>
         <ListaTareas></ListaTareas>
