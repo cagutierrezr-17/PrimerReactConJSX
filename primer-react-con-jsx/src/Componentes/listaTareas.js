76e8c5cdf4bc077bdb19db5e873698b3dbb3028f
Carlos Gutierrez <cagutierrezr.17@icloud.com>
2023-10-03 23:48:59 -0300

    Primer React con JSX
    Tarea Requerida - Primer React con JSX

---
+++ primer-react-con-jsx/src/Componentes/listaTareas.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./listaTareas.css"
+
+class ListaTareas extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Things I need to do:</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default ListaTareas;
